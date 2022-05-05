c9964c1909d9f8ad4caf2d043e0fa3a64c0776cd
ArvikVan <arvikvan@gmail.com>
2022-05-05 15:10:17 +0300

    initPr

---
+++ src/main/java/arv/springdockerrabbitmq/SpringDockerRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package arv.springdockerrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerRabbitmqApplication.class, args);
+	}
+
+}
